8354cca4962c58ecf15f0141a7c50dfb679af393
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2026-08-14 19:17:27 +0700

    fix: failed file writing in vercel environment

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
 
+$directories = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($directories as $directory) {
+    if (!is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
