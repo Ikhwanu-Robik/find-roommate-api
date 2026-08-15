abb66254c5d3dc7f9c53142d0461246dc38ac2ab
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2026-08-15 09:50:52 +0700

    feat: use dummy profile pic

--- app/Http/Controllers/CustomerProfileController.php
+++ app/Http/Controllers/CustomerProfileController.php
@@ -26,14 +26,8 @@ class CustomerProfileController extends Controller
             ], 403);
         }
 
-        $validated = $request->validated();
-        $pathToImage = $request->file('profile_photo')->store('profile_pics');
-        if ($pathToImage === false) {
-            return response()->json([
-                'message' => 'Failed to store profile photo'
-            ], 500);
-        }
-        $validated['profile_photo'] = $pathToImage;
+        // using dummy image for MVP
+        $validated['profile_photo'] = 'profile_pic_male.png';
 
         $customerProfile = CustomerProfile::make($validated);
         $tags = $tagsGenerator->generate($customerProfile->bio);
